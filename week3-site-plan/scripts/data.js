9f59ecbdf7329334792e6f3c9f31bc36e4f6b13d
farai <fmugijima8@gmail.com>
2022-11-18 18:45:58 +0200

    me

--- week3-site-plan/scripts/data.js
+++ week3-site-plan/scripts/data.js
@@ -45,4 +45,20 @@ fetch(requestFile)
     
     // Append the cards
     document.querySelector('div.cards').appendChild(cards);
-  }
+}
+  
+/*------------Button Listeners ----------------*/
+const gridbutton = document.querySelector("#grid");
+const listbutton = document.querySelector("#list");
+const display = document.querySelector("article");
+
+gridbutton.addEventListener("click", () => {
+	// example using arrow function
+	display.classList.add("grid");
+	display.classList.remove("list");
+});
+
+listbutton.addEventListener("click", () => {
+	display.classList.add("list");
+	display.classList.remove("grid");
+});
